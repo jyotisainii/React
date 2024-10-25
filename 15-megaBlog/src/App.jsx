b365956110a8302025d298af27bb796e2af29754
jyotisainii <jyotisaini5614@gmail.com>
2024-10-25 16:17:57 +0530

    save mega project

---
+++ 15-megaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+  return (
+    <>
+      <div>
+        <h1>Hello React</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
---
+++ 15-megaBlog/src/config/config.js
@@ -0,0 +1,10 @@
+const config={
+    appwriteUrl:String(import.meta.env.VITE_APP_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabasesId:String(import.meta.env.VITE_DATABASES_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.VITE_BUCKET_ID),
+
+
+}
+export default config 
