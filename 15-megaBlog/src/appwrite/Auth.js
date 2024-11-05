db742faed247265e7b5b5b38c8b3e958e200eb9e
jyotisainii <jyotisaini5614@gmail.com>
2024-11-05 20:08:40 +0530

    mega project

--- 15-megaBlog/src/appwrite/Auth.js
+++ 15-megaBlog/src/appwrite/Auth.js
@@ -1,5 +1,5 @@
-import {config} from '../config/config.js';
-import {Client,Account,ID} from     "appwrite";
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
 
 
 export class AuthService {
@@ -7,59 +7,56 @@ export class AuthService {
     account;
 
     constructor() {
-        this.client.setEndpoint(config.appwriteUrl);
-        this.client.setProject(config.appwriteProjectId);
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
+            
     }
-   
-async createAccount({email, password,name}){
-        try{
-   const userAccount = await this.account.create(ID.unique(), email, password,name)
-   if(userAccount){
-    return this.login({email, password})
 
-   }else{
-    return userAccount;
-   }
-
-        }catch(error){
-            throw  error;
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            throw error;
         }
     }
 
-
-    
-    async login({email, password}){
-        try{
-           return await  this.account.createEmailPasswordSession(email, password);
-
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
         }
-        catch(error){}
     }
 
     async getCurrentUser() {
-        try{
-        await this.account.get();
-        }catch(error)
-        {
-         return error;
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
+
         return null;
-
     }
-   async logout(){
-    try{
-        await this.account.deleteSessions("current");
-    }catch(error){}
- 
-   }
 
+    async logout() {
 
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
 }
 
+const authService = new AuthService();
 
+export default authService;
 
-
-
-const authService= new AuthService();
-export default authService
--- 15-megaBlog/src/main.jsx
+++ 15-megaBlog/src/main.jsx
@@ -1,10 +1,86 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './page/Home.jsx'
+import { AuthLayout, Login } from './components/index.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+import AddPost from "./page/AddPost.jsx";
+import Signup from './page/SignUp.jsx'
+import EditPost from "./page/EditPost.jsx";
+
+import Post from "./page/Post.jsx";
+
+import AllPost from "./page/AllPost.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Login />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Signup />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AddPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <RouterProvider router={router}/>
+    </Provider>
+  </React.StrictMode>,
 )
